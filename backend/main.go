dbae5ec497d56b298128595803b86c758417a138
jupitters <sampaiobale@gmail.com>
2025-10-20 17:29:15 -0300

    endpoints

--- backend/main.go
+++ backend/main.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"os"
 
+	"github.com/gorilla/mux"
 	_ "github.com/lib/pq"
 )
 
@@ -25,4 +26,11 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	router := mux.NewRouter()
+	router.HandleFunc("/api/go/users", getUsers(db)).Methods("GET")
+	router.HandleFunc("/api/go/users", createUser(db)).Methods("POST")
+	router.HandleFunc("/api/go/users/{id}", getUser(db)).Methods("GET")
+	router.HandleFunc("/api/go/users/{id}", updateUser(db)).Methods("PUT")
+	router.HandleFunc("/api/go/users/{id}", deleteUser(db)).Methods("DELETE")
 }
