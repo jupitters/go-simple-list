a9e78269cd58de818a0f179818463a6a5b1c4885
jupitters <sampaiobale@gmail.com>
2025-10-20 17:01:15 -0300

    create table

--- backend/main.go
+++ backend/main.go
@@ -21,4 +21,8 @@ func main() {
 	}
 	defer db.Close()
 
+	_, err = db.Exec("CREATE TABLE IF NOT EXISTS users (id SERIAL PRIMARY KEY, name TEXT, email TEXT)")
+	if err != nil {
+		log.Fatal(err)
+	}
 }
