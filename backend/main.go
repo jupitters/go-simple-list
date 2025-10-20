35b82de17cfc4c3b9ab7e849ede8ea7d78749cc9
jupitters <sampaiobale@gmail.com>
2025-10-20 16:46:24 -0300

    starting db

--- backend/main.go
+++ backend/main.go
@@ -1,9 +1,24 @@
 package main
 
 import (
+	"database/sql"
+	"log"
+	"os"
+
 	_ "github.com/lib/pq"
 )
 
+type User struct {
+	Id    int    `json:"id"`
+	Name  string `json:"name"`
+	Email string `json:"email"`
+}
+
 func main() {
+	db, err := sql.Open("postgres", os.Getenv("DATABASE_URL"))
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
 
 }
