62a70b3e3ce6c58f6c21e0eebd3af298f1e1dd2c
jupitters <sampaiobale@gmail.com>
2025-10-22 17:26:20 -0300

    funcs

--- backend/main.go
+++ backend/main.go
@@ -56,28 +56,11 @@ func enableCORS(next http.Handler) http.Handler {
 	})
 }
 
-func getUsers(db *sql.DB) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		rows, err := db.Query("SELECT * FROM users")
-		if err != nil {
-			log.Fatal(err)
-		}
-		defer rows.Close()
-
-		users := []User{}
-		for rows.Next() {
-			var u User
-			if err := rows.Scan(&u.Id, &u.Name, &u.Email); err != nil {
-				log.Fatal(err)
-			}
-			users = append(users, u)
-		}
-		if err := rows.Err(); err != nil {
-			log.Fatal(err)
-		}
-
-		json.NewEncoder(w).Encode(users)
-	}
+func jsonContentTypeMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		next.ServeHTTP(w, r)
+	})
 }
 
 func createUser(db *sql.DB) http.HandlerFunc {
