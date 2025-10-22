372d68a44c1694a2c9b21a68a65458223c981e59
jupitters <sampaiobale@gmail.com>
2025-10-22 17:27:20 -0300

    funcs

--- backend/main.go
+++ backend/main.go
@@ -115,3 +115,25 @@ func updateUser(db *sql.DB) http.HandlerFunc {
 		json.NewEncoder(w).Encode(updatedUser)
 	}
 }
+
+func deleteUser(db *sql.DB) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		vars := mux.Vars(r)
+		id := vars["id"]
+
+		var u User
+		err := db.QueryRow("SELECT * FROM users WHERE id = $1", id).Scan(&u.Id, &u.Name, &u.Email)
+		if err != nil {
+			w.WriteHeader(http.StatusNotFound)
+			return
+		} else {
+			_, err := db.Exec("DELETE FROM users WHERE id = ?", id)
+			if err != nil {
+				w.WriteHeader(http.StatusNotFound)
+				return
+			}
+
+			json.NewEncoder(w).Encode("Deleted!")
+		}
+	}
+}
