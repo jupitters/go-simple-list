d334ae344ed80f11541dd5bb65436f0c4ce43169
jupitters <sampaiobale@gmail.com>
2025-10-22 16:17:35 -0300

    fix query

--- backend/main.go
+++ backend/main.go
@@ -92,7 +92,7 @@ func createUser(db *sql.DB) http.HandlerFunc {
 		var u User
 		json.NewDecoder(r.Body).Decode(&u)
 
-		err := db.QueryRow("INSRT INTO users (name, email) VALUES ($1, $2) RETURNING id", u.Name, u.Email).Scan(&u.Id)
+		err := db.QueryRow("INSERT INTO users (name, email) VALUES ($1, $2) RETURNING id", u.Name, u.Email).Scan(&u.Id)
 		if err != nil {
 			log.Fatal(err)
 		}
