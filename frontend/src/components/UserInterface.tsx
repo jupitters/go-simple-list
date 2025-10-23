e30c7f6884b251dcb9a7ed2eff47b8cdcfae248b
jupitters <sampaiobale@gmail.com>
2025-10-23 14:51:54 -0300

    user interface

--- frontend/src/components/UserInterface.tsx
+++ frontend/src/components/UserInterface.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 interface User {
     id: number;
@@ -11,9 +11,10 @@ interface UserInterfaceProps {
 }
 
 const UserInterface: React.FC<UserInterfaceProps> = ({ backendName }) => {
-  return (
-    <div>UserInterface</div>
-  )
+  const apuUrl = process.env.NEXT_PUBLIC_API_URL || "http://localhost:8080";
+  const [users, setUsers] = useState<User[]>([]);
+  const [newUser, setNewUser] = useState({name: '', email: ''});
+  const [updateUser, setUpdateUser] = useState({id: '', name: '', email: ''});
 }
 
 export default UserInterface
