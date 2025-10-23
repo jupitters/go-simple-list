2196038dbd51c9e5339455e4db0a4525823f5299
jupitters <sampaiobale@gmail.com>
2025-10-23 16:10:14 -0300

    home

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -2,9 +2,11 @@ import UserInterface from "@/components/UserInterface";
 
 const Home: React.FC = () => {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
-      <UserInterface backendName="go" />
-    </div>
+    <main className="flex flex-wrap justify-center items-start min-h-screen py-2">
+      <div className="m-4">
+        <UserInterface backendName="go" />
+      </div>
+    </main>
   )
 }
 
