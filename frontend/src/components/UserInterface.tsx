3e3f682fc54b59969b0919a2b06e02b538f3702d
jupitters <sampaiobale@gmail.com>
2025-10-24 15:22:00 -0300

    new image

--- frontend/src/components/UserInterface.tsx
+++ frontend/src/components/UserInterface.tsx
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react'
+import Image from 'next/image'
 import CardComponent from './CardComponent';
 
 interface User {
@@ -13,7 +14,7 @@ interface UserInterfaceProps {
 }
 
 const UserInterface: React.FC<UserInterfaceProps> = ({ backendName }) => {
-  const apiUrl = process.env.NEXT_PUBLIC_API_URL || "http://localhost:8080";
+  const apiUrl = process.env.NEXT_PUBLIC_API_URL || "http://localhost:8000";
   const [users, setUsers] = useState<User[]>([]);
   const [newUser, setNewUser] = useState({name: '', email: ''});
   const [updateUser, setUpdateUser] = useState({id: '', name: '', email: ''});
@@ -43,16 +44,16 @@ const UserInterface: React.FC<UserInterfaceProps> = ({ backendName }) => {
 
   return (
     <div className={`userInterface ${bgColor} ${backendName} w-full max-w-md p-4 my-4 rounded shadow`}>
-        <img src={`/${backendName}logo.svg`} alt={`${backendName} Logo`} className='w-20 h-20 mb-6 mx-auto' />
+        <Image src={`/${backendName}logo.svg`} width={80} height={80} alt={`${backendName} Logo`} className='w-20 h-20 mb-6 mx-auto' />
         <h2 className="text-x1 font-bold text-center text-white mb-6">{`${backendName.charAt(0).toUpperCase() + backendName.slice(1)} Backend`}</h2>
 
         <div className='space-y-4'>
             {users.map((user) => (
                 <div key={user.id} className='flex items-center justify-between bg-white p-4 rounded shadow'>
                     <CardComponent card={user} />
-                    <button onClick={() => deleteUser(user.id)} className={`${btnColor } text-white py-2 px-4 rounded`}>
+                    {/* <button onClick={() => deleteUser(user.id)} className={`${btnColor } text-white py-2 px-4 rounded`}>
                         Delete User
-                    </button>
+                    </button> */}
                 </div>
             ))}
         </div>
