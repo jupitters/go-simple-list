6d03167386bf60c1d95e41000d11d6bb9fa76ff4
jupitters <sampaiobale@gmail.com>
2025-10-26 16:10:32 -0300

    user interface

--- frontend/src/components/UserInterface.tsx
+++ frontend/src/components/UserInterface.tsx
@@ -72,7 +72,14 @@ const UserInterface: React.FC<UserInterfaceProps> = ({ backendName }) => {
     }
   };
 
-  
+  const deleteUser = async (userId: number) => {
+    try{
+      await axios.delete(`${apiUrl}/api/${backendName}/users/${userId}`);
+      setUsers(users.filter((user) => user.id != userId));
+    } catch (error) {
+      console.error("Error deleting user: ", error);
+    }
+  }
 
   return (
     <div className={`userInterface ${bgColor} ${backendName} w-full max-w-md p-4 my-4 rounded shadow`}>
