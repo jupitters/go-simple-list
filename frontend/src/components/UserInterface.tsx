4982f24afec68f3aae8cae3c82ed263d06a6fab7
jupitters <sampaiobale@gmail.com>
2025-10-26 16:09:13 -0300

    user interface

--- frontend/src/components/UserInterface.tsx
+++ frontend/src/components/UserInterface.tsx
@@ -13,10 +13,7 @@ interface UserInterfaceProps {
 }
 
 const UserInterface: React.FC<UserInterfaceProps> = ({ backendName }) => {
-  const apiUrl = process.env.NEXT_PUBLIC_API_URL || "http://localhost:8000";
-  const [users, setUsers] = useState<User[]>([]);
-  const [newUser, setNewUser] = useState({name: '', email: ''});
-  const [updateUser, setUpdateUser] = useState({id: '', name: '', email: ''});
+ 
 
   const backgroundColors: { [key: string]: string} = {
     go: 'bg-cyan-500',
